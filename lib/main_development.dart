1ec9e02b35a8cb7f8d6b3007101336330e13e215
Hassan Kamal AlMosadder <76610671+Hassank455@users.noreply.github.com>
2024-05-19 11:08:21 +0300

    - add Development & Production flavors for Android - add launch.json for flavors config - add two main file for each flavors

---
+++ lib/main_development.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hk4_health/core/di/dependency_injection.dart';
+import 'package:hk4_health/core/routing/app_router.dart';
+import 'package:hk4_health/hk4_health_app.dart';
+
+// dart run build_runner build  --delete-conflicting-outputs
+// flutter run --release -t lib/main_production.dart --flavor Production
+
+void main() async{
+  setupGetIt();
+  // To fix texts being hidden bug in flutter_screenutil in release mode.
+  await ScreenUtil.ensureScreenSize();
+  runApp(Hk4HealthApp(appRouter: AppRouter()));
+}
